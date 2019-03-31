efb15b1e4440b5d2be13cf2fe86f236e13445496
valery <valera-krylov96@mail.ru>
2019-03-31 23:17:58 +0600

    lab2_events

---
+++ lab2/lab2/CheckPointInfo.cs
@@ -0,0 +1,11 @@
+﻿// класс для сбора инфоции о проезжающем ТС
+public class CheckPointInfo
+{
+    public int CurrentSpeed { get; set; }
+    public string MessageOverSpeed { get; set; }
+    public string MessageStolen { get; set; }
+    public string BodyTypeMessage { get; set; }
+    public string LicencePlateNumber { get; set; }
+    public string Color { get; set; }
+    public bool HasPassenger { get; set; }
+}
