e6caf5e7efdb4e424d99c5f2ff3bf1e4b68cb191
valery <valera-krylov96@mail.ru>
2019-04-01 20:50:04 +0600

    lab3_release

--- lab3/lab3/CoffeeMachine.cs
+++ lab3/lab3/CoffeeMachine.cs
@@ -10,26 +10,11 @@ namespace lab3
 
         public CoffeeMachine(Dictionary<CoffeeSelection, CoffeeBean> beans)
         {
-            //_beans = new Dictionary<CoffeeSelection, CoffeeBean>(beans);
             _beans = beans;
             _grinderUnit = new GrinderUnit();
             _brewingUnit = new BrewingUnit();
         }
-        
-        ////add a beans to coffee Machine
-        //public bool addBeas(CoffeeSelection coffeeSelection, CoffeeBean coffeeBean)
-        //{   
-        //    // check have if same key
-        //    if (_beans.ContainsKey(coffeeSelection))
-        //    {   
-        //        //remove old record
-        //        _beans.Remove(coffeeSelection);
-        //        _beans.Add(coffeeSelection, coffeeBean);
-        //        return true;
-        //    }
-        //    return false;
-        //}
-
+       
         public Coffee BrewCoffee (CoffeeSelection coffeeSelection, int guantity)
         {   
             //get for key CoffeeBean -> get GroundCoffee -> get Coffee
