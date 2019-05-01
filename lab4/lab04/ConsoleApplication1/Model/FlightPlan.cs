3e980412ecfbb23f63f9e36ba1780ed585999137
valery <valera-krylov96@mail.ru>
2019-05-01 21:43:57 +0600

    lab4_SpacePort_complete

---
+++ lab4/lab04/ConsoleApplication1/Model/FlightPlan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceFleet.Model
+{
+    class FlightPlan
+    {
+        private Dictionary<String, Double> _distanceToPlanets = new Dictionary<String, Double>();
+
+        public Dictionary<String, Double> getDistanceToPlanets()
+        {
+            return _distanceToPlanets;
+        }
+
+        public void EvaluateDistanceToPlanets(List<Planet> planets)
+        {
+            double xEarth;
+            double distance;
+
+            IEnumerator<Planet> enumerator = planets.GetEnumerator();
+            enumerator.MoveNext();
+            xEarth = enumerator.Current.X;
+
+            while(enumerator.MoveNext())
+            {
+                //evaluate distance 
+                distance = Math.Sqrt(Math.Pow(enumerator.Current.X - xEarth, 2) + Math.Pow(enumerator.Current.Y, 2));
+                _distanceToPlanets.Add(enumerator.Current.Name, distance);
+            }
+            
+        }
+    }
+}
---
+++ lab4/lab04/ConsoleApplication1/Model/SpacePort.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceFleet.Model
+{
+    class SpacePort
+    {
+        private static FlightPlan flightPlan = new FlightPlan();
+        private static List<NearRangeShip> nearRangeShips = new List<NearRangeShip>();
+        private static List<MiddleRangeShip> middleRangeShips = new List<MiddleRangeShip>();
+        private static List<LongRangeShip> longRangeShips = new List<LongRangeShip>();
+        private const double LONG_RANGE = 0.001;
+        private const double MIDDLE_RANGE = 0.0001;
+        private const double NEAR_RANGE = 0.00003;
+        private static int cargoByShip;
+
+        //define matching for light ship
+        public static void LaunchOfShips (List<Planet> planets, List<ASpaceship> ships)
+        {
+            ToDistributeOfShipsByType(ships);
+            flightPlan.EvaluateDistanceToPlanets(planets);
+
+            Dictionary<String, Double> distanceToPlanets = flightPlan.getDistanceToPlanets();
+            double distance;
+
+            //rotate planets 
+            foreach (Planet planet in planets)
+            {
+                //get distance to planet
+                distanceToPlanets.TryGetValue(planet.Name, out distance);
+
+                while (planet.Need > 0)
+                {
+                    if (distance < NEAR_RANGE)
+                    {
+                        if(nearRangeShips.Count > 0)
+                        {
+                           NearRangeShip ship = nearRangeShips.ElementAt(0);
+                           cargoByShip = CalculateCargo(ship.Capacity, planet.Need);
+                           planet.Need -= cargoByShip;
+                           nearRangeShips.Remove(ship);
+                           continue;
+                        }
+                        else if (middleRangeShips.Count > 0)
+                        {
+                           MiddleRangeShip ship = middleRangeShips.ElementAt(0);
+                           cargoByShip = CalculateCargo(ship.Capacity, planet.Need);
+                           planet.Need -= cargoByShip;
+                           middleRangeShips.Remove(ship);
+                           continue;
+                        }
+                        else if (longRangeShips.Count > 0)
+                        {
+                           LongRangeShip ship = longRangeShips.ElementAt(0);
+                           cargoByShip = CalculateCargo(ship.Capacity, planet.Need);
+                           planet.Need -= cargoByShip;
+                           longRangeShips.Remove(ship);
+                           continue;
+                        }
+                        throw new Exception("pull of ships is empty");
+                    }
+//***************************************************************************************************************
+                    else if (distance < MIDDLE_RANGE)
+                    {
+                        if (middleRangeShips.Count > 0)
+                        {
+                           MiddleRangeShip ship = middleRangeShips.ElementAt(0);
+                           cargoByShip = CalculateCargo(ship.Capacity, planet.Need);
+                           planet.Need -= cargoByShip;
+                           middleRangeShips.Remove(ship);
+                           continue;
+                        }
+                        else if (longRangeShips.Count > 0)
+                        {
+                           LongRangeShip ship = longRangeShips.ElementAt(0);
+                           cargoByShip = CalculateCargo(ship.Capacity, planet.Need);
+                           planet.Need -= cargoByShip;
+                           longRangeShips.Remove(ship);
+                           continue;
+                        }
+                        throw new Exception("pull of ships is empty");
+                    }
+//***************************************************************************************************************                    
+                    else if (distance < LONG_RANGE)
+                    {
+                        if (longRangeShips.Count > 0)
+                        {
+                           LongRangeShip ship = longRangeShips.ElementAt(0);
+                           cargoByShip = CalculateCargo(ship.Capacity, planet.Need);
+                           planet.Need -= cargoByShip;
+                           longRangeShips.Remove(ship);
+                           continue;
+                        }
+                        throw new Exception("pull of ships is empty");
+                    }
+
+                }
+            }
+        }
+
+        //difine count transported cargo
+        private static int CalculateCargo(int capasity, int need)
+        {
+            if (capasity <= need)
+                return capasity;
+            else return need;
+        }
+
+        //distribution of ships by type
+        private static void ToDistributeOfShipsByType (List<ASpaceship> ships)
+        {
+            foreach (ASpaceship ship in ships)
+            {
+                if (ship is NearRangeShip)
+                {
+                    nearRangeShips.Add(ship as NearRangeShip);
+                    continue;
+                }
+                else if (ship is MiddleRangeShip)
+                {
+                    middleRangeShips.Add(ship as MiddleRangeShip);
+                    continue;
+                }
+                else if (ship is LongRangeShip)
+                {
+                    longRangeShips.Add(ship as LongRangeShip);
+                    continue;
+                }
+            }
+            nearRangeShips.Sort((object1, object2) => object1.Capacity.CompareTo(object2.Capacity));
+            middleRangeShips.Sort((object1, object2) => object1.Capacity.CompareTo(object2.Capacity));
+            longRangeShips.Sort((object1, object2) => object1.Capacity.CompareTo(object2.Capacity));
+
+        }
+    }
+}
