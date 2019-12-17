730be62713771081eb28b059a638b324e79a32ad
valery <valera-krylov96@mail.ru>
2019-12-17 22:11:54 +0600

    vkr

---
+++ учет ВКР/Учет_ВКР/Учет_ВКР/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Учет_ВКР
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(302, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(701, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите значение критерия для поиска";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Получить список работ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column9,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 162);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(991, 177);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Студент";
+            this.Column9.Name = "Column9";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Название работы";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Дата приказа";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Название приказа";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Руководитель";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Акт внедрения";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Организация";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Дата защиты";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Оценка";
+            this.Column8.Name = "Column8";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(245, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Поиск по руководителю";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(478, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(195, 39);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Поиск по студенту";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(713, 73);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(290, 39);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Поиск по дате защиты (ГГГГ-ММ-ДД)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(12, 392);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(117, 68);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Отчет по группе";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(157, 518);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(336, 392);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(667, 228);
+            this.listBox1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label2.Location = new System.Drawing.Point(154, 400);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 94);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Введите в поле ниже группу, руководителя или дату в формате ГГГГ-ММ-ДД";
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(12, 479);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(117, 68);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Отчет по руководителю";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(12, 562);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(117, 68);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Отчет по дате защиты";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label3.Location = new System.Drawing.Point(417, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(522, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Поле формирования отчета (группа, руководитель, дата защиты)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1015, 660);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Учет ВКР";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ учет ВКР/Учет_ВКР/Учет_ВКР/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Учет_ВКР
+{
+
+    public partial class Form1 : Form
+    {
+        public static string connectString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=VKR_DATABASE.mdb;";
+
+        private OleDbConnection myConnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // создаем экземпляр класса OleDbConnection
+            myConnection = new OleDbConnection(connectString);
+            // открываем соединение с БД
+            myConnection.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myConnection.Close();
+        }
+
+        //получаем все данные
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // текст запроса
+            string query = "SELECT student, title, data_order, title_order, supervisor, act_implementation, organization, data_protection, rate FROM vkr_table ORDER BY title";
+            GetData(query);
+        }
+
+        //поиск по руководителю
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string key = textBox1.Text;
+            string query = "SELECT student, title, data_order, title_order, supervisor, act_implementation, organization, data_protection, rate FROM vkr_table WHERE supervisor = " + "'" + key + "'";
+            GetData(query);
+        }
+
+        //поиск по студенту
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string key = textBox1.Text;
+            string query = "SELECT student, title, data_order, title_order, supervisor, act_implementation, organization, data_protection, rate FROM vkr_table WHERE student = " + "'" + key + "'";
+            GetData(query); 
+        }
+
+        //поиск по дате защиты
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string key = "#" + textBox1.Text + "#";   //  #YYYY-MM-DD#
+            
+            if(textBox1.Text == "")
+            {
+                button1_Click(sender, e);
+                return;
+            }
+
+            string query = "SELECT student, title, data_order, title_order, supervisor, act_implementation, organization, data_protection, rate FROM vkr_table WHERE data_protection = " + key;  
+            GetData(query);   
+        }
+       
+        //отчет по группе
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string key = textBox2.Text;
+            string query = "SELECT student, title_order, rate, act_implementation, data_protection FROM vkr_table WHERE students_group = " + "'" + key + "'";
+            GetReport(query); 
+        }
+
+        //отчет по предподавателю
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string key = textBox2.Text;
+            string query = "SELECT student, title_order, rate, act_implementation, data_protection FROM vkr_table WHERE supervisor = " + "'" + key + "'";
+            GetReport(query); 
+        }
+
+        //отчет по дате защиты
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string key = "#" + textBox2.Text + "#";   //  #YYYY-MM-DD#
+            string query = "SELECT student, title_order, rate, act_implementation, data_protection FROM vkr_table WHERE data_protection = " + key; 
+            GetReport(query);
+        }
+
+        private void GetData(string query)
+        {
+            try
+            {
+                // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+                OleDbCommand command = new OleDbCommand(query, myConnection);
+
+                // получаем объект OleDbDataReader для чтения табличного результата запроса SELECT
+                OleDbDataReader reader = command.ExecuteReader();
+
+                dataGridView1.Rows.Clear();
+                dataGridView1.Refresh();
+
+                // в цикле построчно читаем ответ от БД
+                while (reader.Read())
+                {
+                    string[] rows = { reader[0].ToString(), reader[1].ToString(), reader[2].ToString().Substring(0, 10), reader[3].ToString(),
+                                        reader[4].ToString(),  reader[5].ToString(), reader[6].ToString(), reader[7].ToString().Substring(0, 10), reader[8].ToString() };
+                    dataGridView1.Rows.Add(rows);
+                }
+
+                // закрываем OleDbDataReader
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                GetData("SELECT student, title, data_order, title_order, supervisor, act_implementation, organization, data_protection, rate FROM vkr_table ORDER BY title");
+            }
+            
+        }
+
+        private void GetReport(string query)
+        {
+            try
+            {
+                // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+                OleDbCommand command = new OleDbCommand(query, myConnection);
+
+                // получаем объект OleDbDataReader для чтения табличного результата запроса SELECT
+                OleDbDataReader reader = command.ExecuteReader();
+
+                listBox1.Items.Clear();
+
+                if (!reader.HasRows)
+                {
+                    listBox1.Items.Add(" Данные отсутствуют!");
+                    return;
+                }
+
+                // в цикле построчно читаем ответ от БД
+                while (reader.Read())
+                {
+                    listBox1.Items.Add("Студент:                               " + reader[0].ToString());
+                    listBox1.Items.Add("Название работы:          " + reader[1].ToString());
+                    listBox1.Items.Add("Оценка:                                " + reader[2].ToString());
+                    listBox1.Items.Add("Акт внедрения:                " + reader[3].ToString());
+                    listBox1.Items.Add("Дата защиты:                    " + reader[4].ToString().Substring(0, 10));
+                    listBox1.Items.Add("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                    listBox1.Items.Add("");
+                }
+
+                // закрываем OleDbDataReader
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add(" Пожалуйста, введите корректные данные!");
+            }
+           
+        }
+
+    }
+}
